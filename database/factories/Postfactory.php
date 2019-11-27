5b2fb817f941d53130d48ff24910635dcfa05e04
Rajan <rajan@freshbits.in>
2019-11-27 17:56:52 +0530

    image url change for seed
    Signed-off-by: Rajan <rajan@freshbits.in>

--- database/factories/Postfactory.php
+++ database/factories/Postfactory.php
@@ -6,11 +6,11 @@ use App\Post;
 use Faker\Generator as Faker;
 
 $urlArray = [
-    'https://cdn.pixabay.com/photo/2016/09/14/22/40/landscape-1670702_960_720.jpg',
-    'https://www.maxpixel.net/static/photo/1x/Landscape-Nature-Natural-Ski-Mountain-Mammoth-2282027.jpg',
+    'https://cdn.pixabay.com/photo/2017/01/30/16/11/sunset-2021266_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2017/09/29/22/55/panorama-2800783_960_720.jpg',
     'https://cdn.pixabay.com/photo/2018/01/31/06/51/waters-3120408_960_720.jpg',
-    'https://www.maxpixel.net/static/photo/1x/Lake-Mountain-Park-Grass-Natural-Beach-Forest-2783999.jpg',
-    'https://www.maxpixel.net/static/photo/1x/Stone-Desert-Nature-Rocks-Wall-Landmark-Natural-1304148.jpg'
+    'https://cdn.pixabay.com/photo/2019/08/14/18/12/landscape-4406387_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2016/08/26/17/33/landscape-1622739_960_720.jpg'
 ];
 
 $factory->define(Post::class, function (Faker $faker) use($urlArray) {
