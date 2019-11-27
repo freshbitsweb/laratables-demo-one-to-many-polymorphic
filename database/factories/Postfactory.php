3993c680c0e5b34e7b46631a68ff70288e33863d
Rajan <rajan@freshbits.in>
2019-11-27 15:17:11 +0530

    custom image url seed
    Signed-off-by: Rajan <rajan@freshbits.in>

--- database/factories/Postfactory.php
+++ database/factories/Postfactory.php
@@ -5,11 +5,19 @@
 use App\Post;
 use Faker\Generator as Faker;
 
-$factory->define(Post::class, function (Faker $faker) {
+$urlArray = [
+    'https://cdn.pixabay.com/photo/2016/09/14/22/40/landscape-1670702_960_720.jpg',
+    'https://www.maxpixel.net/static/photo/1x/Landscape-Nature-Natural-Ski-Mountain-Mammoth-2282027.jpg',
+    'https://cdn.pixabay.com/photo/2018/01/31/06/51/waters-3120408_960_720.jpg',
+    'https://www.maxpixel.net/static/photo/1x/Lake-Mountain-Park-Grass-Natural-Beach-Forest-2783999.jpg',
+    'https://www.maxpixel.net/static/photo/1x/Stone-Desert-Nature-Rocks-Wall-Landmark-Natural-1304148.jpg'
+];
+
+$factory->define(Post::class, function (Faker $faker) use($urlArray) {
     return [
-        'title' => $faker->sentence($nbWords = 1, $variableNbWords = true),
-        'url' => $faker->url,
-    ];
+            'title' => $faker->sentence($nbWords = 1, $variableNbWords = true),
+            'url' => $urlArray[array_rand($urlArray)],
+        ];
 });
 
 $factory->afterCreating(Post::class, function ($post, $faker) {
